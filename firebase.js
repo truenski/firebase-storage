4d8a83aa157a3b7a9a246170de8df47cb4f39159
Truenski <kesneymendes@gmail.com>
2022-02-20 10:34:12 -0300

    Initial commit

---
+++ firebase.js
@@ -0,0 +1,16 @@
+ 
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCbq6y28bDu_CE0n60NS8-hvovwHzq-5QA",
+  authDomain: "olhando-so.firebaseapp.com",
+  projectId: "olhando-so",
+  storageBucket: "olhando-so.appspot.com",
+  messagingSenderId: "837345975759",
+  appId: "1:837345975759:web:88f2ace3a45f20a7a106ba"
+};
+
+
+
+
+const storage = firebase.storage();
+const ref = storage.ref("/lalau");
